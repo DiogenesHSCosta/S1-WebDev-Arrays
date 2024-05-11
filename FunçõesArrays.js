4bb03e839c00dd08076089741b5fbd2d5f11aba4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 22:44:12 -0300

    Funções com arrays

--- FunçõesArrays.js
+++ FunçõesArrays.js
@@ -1,18 +1,26 @@
 const pessoas = ['Douglas', 'Felipe', 'MATHEUS']
+const palavras = ['ola', 'bom dia', 'teste']
 
 //percorrer array e não gera um retorno
 pessoas.forEach(e => {
     e = e.toLowerCase()
-
                 //operador ternário(lugar do if)
     console.log(e == 'douglas'? `Bem vindo douglas`: `Você não é o douglas, voce é o ${e}`)
 }
 )
 
 //percorrer array e gerar retorno
-const Mapeada = pessoas.map((e) =>{
+const mapeada = pessoas.map((e) =>{
                 //operador ternario
     return e == 'Felipe'? `${e} VAI CORINTHIANS`: `${e} VAI SANTOS`
 })
+console.log(mapeada)
 
-console.log(Mapeada)
+
+//usando map na função
+function uperCase(){
+    return palavras.map(e => e.toUpperCase())
+}
+
+const teste = uperCase() 
+console.log(teste)
