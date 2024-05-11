6ae79d0a5818dea45b797dcb71fc036ec89e8aaf
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 21:51:56 -0300

    inserção de valores no começo e fim do array

---
+++ script.js
@@ -0,0 +1,17 @@
+const pessoas = ['douglas', 'felipe', 'matheus']
+
+//adiciona no ultimo
+pessoas.push('ser humano')
+
+//remove o ultimo item do array
+pessoas.pop()
+
+//adicionar um item na primeira posição do array
+pessoas.unshift()
+
+//remover o primeiro item
+pessoas.shift()
+
+pessoas.map((e)=>{
+    console.log(e)
+})
